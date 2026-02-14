eb2a7e35653eaf88367eb5f9be12c7f424e1b233
takanasensei <fujiya.taiken@gmail.com>
2026-02-14 12:59:27 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/webhook", (req, res) => {
+  console.log("Webhook received:");
+  console.log(JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+app.get("/", (req, res) => {
+  res.send("LINE Webhook running");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
